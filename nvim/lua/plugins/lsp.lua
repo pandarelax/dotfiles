8b9b02d5ee6ee795b7fd1a773a83fdae36fed4b3
Efehan Turhan <efehanturhan@gmail.com>
2025-07-10 18:15:31 +0300

    feat: update lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,12 +1,20 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
     opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
+      servers = {
+        buf_ls = {
+          cmd = {
+            "/nix/store/vnm41rr063lxmfhfgd08cvav5ds1637y-buf-with-plugins-1.52.1/bin/buf",
+            "beta",
+            "lsp",
+            "--timeout",
+            "0",
+            "--log-format=text",
+          },
+          filetypes = { "proto" },
+          root_dir = require("lspconfig.util").root_pattern("buf.work.yaml", "buf.yaml", ".git") or vim.fn.getcwd(),
+        },
       },
     },
   },
